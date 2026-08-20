78eba5c82c6976ea2ac461856f0837150ca858c4
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-20 16:33:33 -0400

    Update grid.js

--- grid.js
+++ grid.js
@@ -76,6 +76,7 @@ function loadGrid(x) {
 function eventInterval(y) {
 	y.interval = setInterval(function() {
 		$('#txtScreen').append('<span style="color: '+ y.color + '">' + y.post + '</span></br>');
+		$("#txtScreen").change($('#txtScreen').scrollTop($('#txtScreen')[0].scrollHeight));
 	}, y.freq);
 }
 
