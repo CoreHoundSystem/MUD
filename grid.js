424940c4a78dad3026d4ad8b6829d242a333d39d
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-21 16:26:41 -0400

    Update grid.js

--- grid.js
+++ grid.js
@@ -85,7 +85,7 @@ function up() {
 	i = grid.paths.indexOf("Up");
 	console.log(i);
 	console.log(grid.pathIDs);
-	loadGrid([grid.pathIDs[i]]);
+	loadGrid(window[grid.pathIDs[i]]);
 }
 //test
 gridA4 = {
