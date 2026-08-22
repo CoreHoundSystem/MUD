e314de96f2f0b59399bc37a84686165a474cd509
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-22 14:24:31 -0400

    Update general.js

--- general.js
+++ general.js
@@ -16,6 +16,7 @@ function command(c) {
 	console.log(c);
 	c = c.split(" ");
 	console.log(c);
+	console.log(c.length);
 	if(c.length == 1) {
 		if(c[0].lowerCase == "heal") {
 			post(char.name + " heals themself!","positiveEnergy");
