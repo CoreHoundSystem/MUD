9398c60dd3f4f79876cbd7226c2f6ccc712bd80f
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-22 14:34:45 -0400

    Update general.js

--- general.js
+++ general.js
@@ -18,39 +18,10 @@ function command(c) {
 	console.log(c);
 	console.log(c[0].toLowerCase());
 	if(c.length == 1) {
+		console.log(c);
 		if(c[0].toLowerCase() == "heal") {
+			console.log(character.name);
 			post(character.name + " heals themself!","positiveEnergy");
-		}
-	}
-
-}
-
-function post(a,b) {
-	$('#txtScreen').append('<span class="' + b + '">' + a + '</span></br>');
-	//this line moves scrollbar to bottom
-	$("#txtScreen").change($('#txtScreen').scrollTop($('#txtScreen')[0].scrollHeight));
-}$("#commandLine").keypress(function (e) {
-	if(e.which === 13 && !e.shiftKey) { //includes option to ignore if SHIFT is held
-		e.preventDefault();
-		console.log($("#commandLine").val());
-		command($("#commandLine").val());
-		//clears command line
-		$("#commandLine").val('');
-	}
-});
-
-char = {
-	name: "Evander",
-}
-
-function command(c) {
-	console.log(c);
-	c = c.split(" ");
-	console.log(c);
-	console.log(c[0].toLowerCase());
-	if(c.length == 1) {
-		if(c[0].toLowerCase() == "heal") {
-			post(char.name + " heals themself!","positiveEnergy");
 		}
 	}
 
