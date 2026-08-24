d4a84f74597dab935dccdc987dbb375a25805f5c
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-24 09:43:08 -0400

    Update grid.js

--- grid.js
+++ grid.js
@@ -1,9 +1,3 @@
-$(function() {
-	$('#txtScreen').scrollTop($('#txtScreen')[0].scrollHeight);
-})
-
-//$("#txtScreen").change($('#txtScreen').scrollTop($('#txtScreen')[0].scrollHeight));
-
 var characters = ["Steve","Percival","Shandryll"];
 var grid = {
 	eList: [""],
@@ -163,6 +157,138 @@ gridB4 = {
 	desc: "The hull of the Breiðr Ormr is broad and has multiple decks - this is the lowest deck. It is dark and humid with every breath adding to the suffocating melancholy of your shipmates.",
 	paths: ["Down","Bow","Stern"],
 	pathIDs: ["gridA4","gridA5","gridA3"],
+	smell: {
+		none: "The air is thick and heavy. The smell of body odor and filth chokes out all other scents except the occassional breeze of crisp ocean air that finds its way down from above.",
+	},
+	look: {
+		none: "All around you are the crestfallen faces of fellow warriors.",
+		up: "The stairs leading to the deck above are the cleanest part of this deck. The deck above seems packed with barrels and crates."
+	},
+	feel: {
+		none: "Your clothes are stained with sweat so thick that the fabric has become rigid and coarse.",
+	},
+	listen: {
+		none: "Despite being surrounded it is eerily silent here. All that can be heard are the waves slapping against the hull and the sound of footsteps from the decks above.",
+		up: "The sounds of sailors walking about can be heard.",
+	},
+	taste: {
+		none: "Your lips are salty with sweat.",
+	},
+	search: {
+		none: "The floor is covered in sea water and filth. There is nothing of value here.",
+	},
+	open: {
+		none: "What would you like to open?",
+	},
+	sit: {
+		none: "You return to your makeshift bench.",
+	},
+	lay: {
+		none: "There is no room to lay down here.",
+	},
+	climb: {
+		none: "You could climb up the stairs to the deck above when you are ready.",
+	},
+	mobs: [
+		"Bilge Rat",
+		"Bilge Rat"
+	],
+	take: [
+		"Soiled scrap of cloth",
+	],
+	eList: [
+		"footsteps",
+		"breeze"
+	],
+	eType: {
+		footsteps: {
+			post: "The sound of footsteps can be heard from the deck above.",
+			freq: 3000,
+			color: "white",
+			interval: "footstepsInterval",
+		},
+		breeze: {
+			post: "A much needed breeze brings crisp ocean air into the hold.",
+			freq: 5000,
+			color: "white",
+			interval: "breezeInterval",
+		},
+	}
+}
+
+
+gridA5 = {
+	name: "Breiðr Ormr - middle deck",
+	map: "Vey'Keng",
+	desc: "The hull of the Breiðr Ormr is broad and has multiple decks - this is the lowest deck. It is dark and humid with every breath adding to the suffocating melancholy of your shipmates.",
+	paths: ["West"],
+	pathIDs: ["gridA4"],
+	smell: {
+		none: "The air is thick and heavy. The smell of body odor and filth chokes out all other scents except the occassional breeze of crisp ocean air that finds its way down from above.",
+	},
+	look: {
+		none: "All around you are the crestfallen faces of fellow warriors.",
+		up: "The stairs leading to the deck above are the cleanest part of this deck. The deck above seems packed with barrels and crates."
+	},
+	feel: {
+		none: "Your clothes are stained with sweat so thick that the fabric has become rigid and coarse.",
+	},
+	listen: {
+		none: "Despite being surrounded it is eerily silent here. All that can be heard are the waves slapping against the hull and the sound of footsteps from the decks above.",
+		up: "The sounds of sailors walking about can be heard.",
+	},
+	taste: {
+		none: "Your lips are salty with sweat.",
+	},
+	search: {
+		none: "The floor is covered in sea water and filth. There is nothing of value here.",
+	},
+	open: {
+		none: "What would you like to open?",
+	},
+	sit: {
+		none: "You return to your makeshift bench.",
+	},
+	lay: {
+		none: "There is no room to lay down here.",
+	},
+	climb: {
+		none: "You could climb up the stairs to the deck above when you are ready.",
+	},
+	mobs: [
+		"Bilge Rat",
+		"Bilge Rat"
+	],
+	take: [
+		"Soiled scrap of cloth",
+	],
+	eList: [
+		"footsteps",
+		"breeze"
+	],
+	eType: {
+		footsteps: {
+			post: "The sound of footsteps can be heard from the deck above.",
+			freq: 3000,
+			color: "white",
+			interval: "footstepsInterval",
+		},
+		breeze: {
+			post: "A much needed breeze brings crisp ocean air into the hold.",
+			freq: 5000,
+			color: "white",
+			interval: "breezeInterval",
+		},
+	}
+}
+
+
+gridA3 = {
+	name: "Breiðr Ormr - middle deck",
+	map: "Vey'Keng",
+	desc: "The hull of the Breiðr Ormr is broad and has multiple decks - this is the lowest deck. It is dark and humid with every breath adding to the suffocating melancholy of your shipmates.",
+	paths: ["East"],
+	pathIDs: ["gridA4"],
 	smell: {
 		none: "The air is thick and heavy. The smell of body odor and filth chokes out all other scents except the occassional breeze of crisp ocean air that finds its way down from above.",
 	},
