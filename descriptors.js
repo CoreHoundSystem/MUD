4b712da4f3a0efef109058cd094a02a7ef24e039
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-27 15:11:51 -0400

    Create descriptors.js

---
+++ descriptors.js
@@ -0,0 +1,14 @@
+attacks = [
+	" lunges with their ",
+	" swings their ",
+]
+
+hits = [
+	" and hits true.",
+	" and strikes a mighty blow.",
+]
+
+miss = [
+	" and narrowly misses.",
+	" and fails to score a hit.",
+]
