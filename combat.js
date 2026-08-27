2933d051a635b47721a8b077fd4ab14d4287e3dd
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-27 14:49:40 -0400

    Update combat.js

--- combat.js
+++ combat.js
@@ -1,40 +1,47 @@
+qSpell = "";		//qued spell
+
 function combat(x,y) {
 	console.log(x);
 	console.log(y);
 	//enter 'combatMode'
+	
 	doBattle = setInterval(function() {
-		cAtt = character.attr.str;
-		cSkill = character.skill.unarmed.rating;
-		if(character.equipped.mainhand !== "") {
-			//build database of weapons and skills
-			console.log(character.equipped.mainhand);
-		}
-		cLvl = character.level;
-		threat = ((cLvl + window[y].level) * .05);
-		console.log(threat);
-		console.log(window[y].level);
-		if(threat == 0) {
-			threat = .05;
-		}
-		console.log(threat);
-		bonus = ((cAtt - cLvl) + (cSkill - (cLvl * 5))) / threat;
-		console.log(bonus);
-		result = "";
-		roll = Math.floor(Math.random()*100)+1;
-		max = 95;	//dual wield and two hander
-		effectiveBonus = 0;
-		if((bonus + 95) > max) { 
-			effectiveBonus = max; 
+		if(qSpell == "") {
+			cAtt = character.attr.str;
+			cSkill = character.skill.unarmed.rating;
+			if(character.equipped.mainhand !== "") {
+				//build database of weapons and skills
+				console.log(character.equipped.mainhand);
+			}
+			cLvl = character.level;
+			threat = ((cLvl + window[y].level) * .05);
+			console.log(threat);
+			console.log(window[y].level);
+			if(threat == 0) {
+				threat = .05;
+			}
+			console.log(threat);
+			bonus = ((cAtt - cLvl) + (cSkill - (cLvl * 5))) / threat;
+			console.log(bonus);
+			result = "";
+			roll = Math.floor(Math.random()*100)+1;
+			max = 95;	//dual wield and two hander
+			effectiveBonus = 0;
+			if((bonus + 95) > max) { 
+				effectiveBonus = max; 
+			} else {
+				effectiveBonus = bonus + 95; 
+			}
+			console.log("Hit chance " + effectiveBonus);
+			if (roll > effectiveBonus) {
+				result = "misses.";
+			} else {
+				result = "hits!";
+			}
+			console.log(roll + " " + result);
 		} else {
-			effectiveBonus = bonus + 95; 
+			post(charcter.name + " uses " + qSpell);
+			qSpell = "";
 		}
-		console.log("Hit chance " + effectiveBonus);
-		if (roll > effectiveBonus) {
-			result = "misses.";
-		} else {
-			result = "hits!";
-		}
-		console.log(roll + " " + result);
-	//start interval
 	}, 3000);
 }
