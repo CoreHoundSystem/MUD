c0206438720d3e4989fd27dd77cd137c56ea0e24
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-27 12:52:13 -0400

    Update general.js

--- general.js
+++ general.js
@@ -41,94 +41,198 @@ bite = {
 }
 
 character = {
-	name: "Evander",
+	name: "",
 	grid: "gridA4",
-	level: 20,
+	level: 1,
+	xp: 0,
 	hp: {
 		current: 1,
-		max: 97,
-		rate: 5,
+		max: 10,
+		rate: 1,	//constitution
 	},
 	rage: {
 		current: 0,
 		max: 100,
-		rate: 0,
+		rate: 0,	//action related
 	},
 	energy: {
 		current: 100,
 		max: 100,
-		rate: 20,
+		rate: 20,	//if changed - charisma
 	},
 	mana: {
 		current: 100,
-		max: 100,
-		rate: 5,
+		max: 100,	//int + wis
+		rate: 1,	//channeling
 	},
 	attr: {
-		str: 21,
-		dex: 40,
-		con: 30,
-		inte: 15,
-		wis: 12,
-		cha: 15,
+		str: 1,
+		dex: 1,
+		con: 1,
+		inte: 1,
+		wis: 1,
+		cha: 1,
 	},
 	skill: {
-		athletics: 105,
-		axe: 105,
-		blunt: 105,
-		channeling: 105,
-		sword: 105,
-		unarmed: 105,
+		athletics: {
+			rating: 1,
+			xp: 0,
+		},
+		axe: {
+			rating: 1,
+			xp: 0,
+		},
+		blunt: {
+			rating: 1,
+			xp: 0,
+		},
+		channeling: {
+			rating: 1,
+			xp: 0,
+		},
+		sword: {
+			rating: 1,
+			xp: 0,
+		},
+		unarmed: {
+			rating: 1,
+			xp: 0,
+		},
 	},
 	sphere: {
-		air: 105,
-		earth: 105,
-		evil: 105,
-		fire: 105,
-		good: 105,
-		healing: 105,
-		magic: 105,
-		purification: 105,
-		undeath: 105,
-		war: 105,
-		water: 105,
+		air: {
+			rating: 1,
+			xp: 0,
+		},
+		earth: {
+			rating: 1,
+			xp: 0,
+		},
+		evil: {
+			rating: 1,
+			xp: 0,
+		},
+		fire: {
+			rating: 1,
+			xp: 0,
+		},
+		good: {
+			rating: 1,
+			xp: 0,
+		},
+		healing: {
+			rating: 1,
+			xp: 0,
+		},
+		magic: {
+			rating: 1,
+			xp: 0,
+		},
+		purification: {
+			rating: 1,
+			xp: 0,
+		},
+		undeath: {
+			rating: 1,
+			xp: 0,
+		},
+		war: {
+			rating: 1,
+			xp: 0,
+		},
+		water: {
+			rating: 1,
+			xp: 0,
+		},
 	},
 	school: {
-		abjuration: 105,
-		alteration: 105,
-		auspex: 105,
-		charm: 105,
-		conjuration: 105,
-		divination: 105,
-		enchantment: 105,
-		evocation: 105,
-		illusion: 105,
-		invocation: 105,
-		necromancy: 105,
-		phantasm: 105,
-		summoning: 105,
-		transmutation: 105,
-		unarmed: 105,
+		abjuration: {
+			rating: 1,
+			xp: 0,
+		},
+		alteration: {
+			rating: 1,
+			xp: 0,
+		},
+		auspex: {
+			rating: 1,
+			xp: 0,
+		},
+		charm: {
+			rating: 1,
+			xp: 0,
+		},
+		conjuration: {
+			rating: 1,
+			xp: 0,
+		},
+		divination: {
+			rating: 1,
+			xp: 0,
+		},
+		enchantment: {
+			rating: 1,
+			xp: 0,
+		},
+		evocation: {
+			rating: 1,
+			xp: 0,
+		},
+		illusion: {
+			rating: 1,
+			xp: 0,
+		},
+		invocation: {
+			rating: 1,
+			xp: 0,
+		},
+		necromancy: {
+			rating: 1,
+			xp: 0,
+		},
+		phantasm: {
+			rating: 1,
+			xp: 0,
+		},
+		summoning: {
+			rating: 1,
+			xp: 0,
+		},
+		transmutation: {
+			rating: 1,
+			xp: 0,
+		},
+		unarmed: {
+			rating: 1,
+			xp: 0,
+		},
 	},
 	equipped: {
-		mainhand: "Longsword",
+		mainhand: "",
 		offhand: "",
 		armor: {
 			helm: "",
 			shoulders: "",
 			chest: "",
 			hands: "",
-			legs: "",
-			feet: "",
+			legs: "Worn linen pants",
+			feet: "Worn leather boots",
 		},
 		cloths: {
 			back: "",
 			neck: "",
-			chest: "",
+			chest: "Sweat stained rags",
 			finger1: "",
 			finger2: "",
 			waist: "",
 		},
+		bags: {
+			one: "Deteriorating knapsack",
+			two: "",
+			three: "",
+			four: "",
+			five: "",
+		}
 	},
 	inventory: [
 		"Scrap of bread",
