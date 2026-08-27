9590dd21202744dde2dfa9f342a11ab146fa2d28
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-27 13:55:29 -0400

    Update combat.js

--- combat.js
+++ combat.js
@@ -3,13 +3,46 @@ function combat(x,y) {
 	//enter 'combatMode'
 	//x = player, y = target
 	//build numbers
-	//start interval
-	threat = ((character.level + bilgeRat.level) * .05);
+	//find att and skill
+	cAtt = x.attr.str;
+	cSkill = x.skill.unarmed.rating;
+	if(x.mainhand !== "") {
+		//build database of weapons and skills
+	}
+	cLvl = x.level;
+	threat = ((cLvl + y.level) * .05);
 	if(threat == 0) {
 		threat = .05;
 	}
-	bonus = ((character.attr.dex - character.level) 
-		+ (character.skill.sword.rating - (character.level * 5)))
+	bonus = ((cAtt - cLvl) + (cSkill - (cLvl * 5))) / threat;
+	
+	result = "";
+	roll = Math.floor(Math.random()*100)+1;
+	max = 95;	//dual wield and two hander
+	effectiveBonus = 0;
+	if((bonus + 95) > max) { 
+		effectiveBonus = max; 
+	} else {
+		effectiveBonus = bonus + 95; 
+	}
+	console.log("Hit chance " + effectiveBonus);
+	if (roll > effectiveBonus) {
+		result = "misses.";
+	} else {
+		result = "hits!";
+	}
+	console.log(roll + " " + result);
+	//start interval
+	
+
+
+	
+	
+	
+	
+	/*
+	bonus = ((character.attr.dex - cLvl) 
+		+ (character.skill.sword.rating - (cLvl * 5)))
 		/ threat;
 	//
 	console.log(character.attr.dex);
@@ -19,27 +52,11 @@ function combat(x,y) {
 	console.log(character.attr.dex - character.level);
 	//console.log((character.attr.dex - character.level) / character.level);
 	//console.log(((character.attr.dex - character.level) / character.level) * 10);
-	console.log(character.skill.sword - (character.level * 5));
+	console.log(character.skill.sword.rating - (character.level * 5));
 	console.log((character.level + bilgeRat.level) * 5);
 	console.log(x.name + " attacks " + y + ".");
 	console.log("Hit chance " + (95 + bonus));
-	result = "";
-	roll = Math.floor(Math.random()*100)+1;
-	baseBonus = bonus + 95;
-	max = 95;
-	effectiveBonus = 0;
-	if(baseBonus > max) { 
-		effectiveBonus = max; 
-	} else {
-		effectiveBonus = baseBonus; 
-	}
-	console.log("Hit chance " + effectiveBonus);
-	if (roll > effectiveBonus) {
-		result = "misses.";
-	} else {
-		result = "hits!";
-	}
-	console.log(roll + " " + result);
+	*/
 
 	//+ Math.floor(Math.random()*100)+1 < y) {
 		
