84ffb13f4a852ed4c73fb58a909748997407a72d
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-27 15:20:12 -0400

    Update combat.js

--- combat.js
+++ combat.js
@@ -6,47 +6,51 @@ function combat(x,y) {
 	//enter 'combatMode'
 	
 	doBattle = setInterval(function() {
-		if(qSpell == "") {
-			cAtt = character.attr.str;
-			cSkill = character.skill.unarmed.rating;
-			if(character.equipped.mainhand !== "") {
-				//build database of weapons and skills
-				console.log(character.equipped.mainhand);
-			}
-			cLvl = character.level;
-			threat = ((cLvl + window[y].level) * .05);
-			console.log(threat);
-			console.log(window[y].level);
-			if(threat == 0) {
-				threat = .05;
-			}
-			console.log(threat);
-			bonus = ((cAtt - cLvl) + (cSkill - (cLvl * 5))) / threat;
-			console.log(bonus);
-			result = "";
-			roll = Math.floor(Math.random()*100)+1;
-			max = 95;	//dual wield and two hander
-			effectiveBonus = 0;
-			if((bonus + 95) > max) { 
-				effectiveBonus = max; 
-			} else {
-				effectiveBonus = bonus + 95; 
-			}
-			console.log("Hit chance " + effectiveBonus);
-			if (roll > effectiveBonus) {
-				result = "misses.";
-				attackRan = attack[Math.floor(Math.random() * miss.length)+1];
-				missRan = miss[Math.floor(Math.random() * miss.length)+1];
-				post(character.name + attackRan + character.equiped.mainhand + missRan,3);
-			} else {
-				result = "hits!";
-				
-			}
-			console.log(roll + " " + result);
+		if(character.hp.current <= 0 || window[y].hp.current <= 0) {
+			//stop
 		} else {
-			post(character.name + " uses " + qSpell,2);	//class should originate from ability
-			qSpell = "";
-			console.log(character);
+			if(qSpell == "") {
+				cAtt = character.attr.str;
+				cSkill = character.skill.unarmed.rating;
+				if(character.equipped.mainhand !== "") {
+					//build database of weapons and skills
+					console.log(character.equipped.mainhand);
+				}
+				cLvl = character.level;
+				threat = ((cLvl + window[y].level) * .05);
+				console.log(threat);
+				console.log(window[y].level);
+				if(threat == 0) {
+					threat = .05;
+				}
+				console.log(threat);
+				bonus = ((cAtt - cLvl) + (cSkill - (cLvl * 5))) / threat;
+				console.log(bonus);
+				result = "";
+				roll = Math.floor(Math.random()*100)+1;
+				max = 95;	//dual wield and two hander
+				effectiveBonus = 0;
+				if((bonus + 95) > max) { 
+					effectiveBonus = max; 
+				} else {
+					effectiveBonus = bonus + 95; 
+				}
+				console.log("Hit chance " + effectiveBonus);
+				if (roll > effectiveBonus) {
+					result = "misses.";
+					attackRan = attack[Math.floor(Math.random() * miss.length)+1];
+					missRan = miss[Math.floor(Math.random() * miss.length)+1];
+					post(character.name + attackRan + character.equiped.mainhand + missRan,3);
+				} else {
+					result = "hits!";
+					
+				}
+				console.log(roll + " " + result);
+			} else {
+				post(character.name + " uses " + qSpell,2);	//class should originate from ability
+				qSpell = "";
+				console.log(character);
+			}
 		}
 	}, 3000);
 }
