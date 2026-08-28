02f803aaf0905ada693377a8a01505aeb1964ce5
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-28 15:00:23 -0400

    Update weapons.js

--- weapons.js
+++ weapons.js
@@ -2,33 +2,38 @@ longsword = {
 	skill: "sword",
 	damage: "1d8",
 	used: "mainhand",
+	parry: 10,
 }
 
 dagger = {
 	skill: "dagger",
 	damage: "1d4",
 	used: "onehand",
+	parry: 5,
 }
 
 parryingDagger = {
 	skill: "dagger",
 	damage: "1d3",
 	used: "offhand",
+	parry: 15,
 }
 
 bite = {
 	skill: "natural",
 	damage: "1d3",
 	used: "twohand",
+	parry: 0,
 }
 
 unarmed = {
 	skill: "unarmed",
 	damage: "1d2",
 	used: "twohand",
+	parry: 0,
 }
 
-unarmed = {
+shield = {
 	skill: "shield",
 	damage: "1d2",
 	used: "shield",
