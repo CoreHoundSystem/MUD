eb43d5b3f960145b6671409efb504d943328cb92
Joshua Hess <51447286+CoreHoundSystem@users.noreply.github.com>
2026-08-28 15:06:50 -0400

    Update weapons.js

--- weapons.js
+++ weapons.js
@@ -3,6 +3,15 @@ longsword = {
 	damage: "1d8",
 	used: "mainhand",
 	parry: 10,
+	speed: 3,
+}
+
+greatsword = {
+	skill: "sword",
+	damage: "2d6",
+	used: "twohand",
+	parry: 20,
+	speed: 5,
 }
 
 dagger = {
@@ -10,6 +19,7 @@ dagger = {
 	damage: "1d4",
 	used: "onehand",
 	parry: 5,
+	speed: 1,
 }
 
 parryingDagger = {
@@ -17,6 +27,7 @@ parryingDagger = {
 	damage: "1d3",
 	used: "offhand",
 	parry: 15,
+	speed: 2,
 }
 
 bite = {
@@ -24,6 +35,7 @@ bite = {
 	damage: "1d3",
 	used: "twohand",
 	parry: 0,
+	speed: 3,
 }
 
 unarmed = {
@@ -31,6 +43,7 @@ unarmed = {
 	damage: "1d2",
 	used: "twohand",
 	parry: 0,
+	speed: 3,
 }
 
 shield = {
@@ -38,4 +51,5 @@ shield = {
 	damage: "1d2",
 	used: "shield",
 	block: 30,
+	speed: 3,
 }
